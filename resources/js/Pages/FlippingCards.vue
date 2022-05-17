921308b3bf0024a68ab56ddb51a1411e664fe82e
osbassam <77623767+osbassam@users.noreply.github.com>
2022-05-18 01:33:29 +0200

    wip

--- resources/js/Pages/FlippingCards.vue
+++ resources/js/Pages/FlippingCards.vue
@@ -1,6 +1,5 @@
 <script setup>
-import Blog from '@/Pages/Blog.vue';
-</script>
+import Blog from '@/Pages/Blog.vue';</script>
 
 <template>
     <Blog>
@@ -15,27 +14,28 @@ import Blog from '@/Pages/Blog.vue';
         <div class="h-full">
             <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 
-                <div id="painterro"></div>
-                <!--                <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">-->
-                <!--                    <div v-for="(card, index) in cards" :key="card.flipped" class="group relative">-->
-                <!--&lt;!&ndash;                        <transition name="flip">&ndash;&gt;-->
-                <!--                        <vue-flip active-click="" width="250px" height="400px">-->
-                <!--                            <template v-slot:front class="front w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1-->
-                <!--                                rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">-->
-                <!--                                <img :src="'../storage/img/' + card.imgSrc" @click="toggleCard(card)" alt="product.imageAlt"-->
-                <!--                                     class="mx-4 w-full h-full object-center object-cover lg:w-full lg:h-full"/>-->
-                <!--                                <span class="">click</span>-->
-                <!--                            </template>-->
-                <!--                            <template v-slot:back class="back w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1-->
-                <!--                                rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">-->
-                <!--                                <img :src="'../storage/img/' + card.imgSrcBack" @click="toggleCard(card)" alt="product.imageAlt"-->
-                <!--                                     class="mx-4 w-full h-full object-center object-cover lg:w-full lg:h-full"/>-->
-                <!--                                <span class=""> click</span>-->
-                <!--                            </template>-->
-                <!--                        </vue-flip>-->
-                <!--&lt;!&ndash;                        </transition>&ndash;&gt;-->
-                <!--                    </div>-->
-                <!--                </div>-->
+                <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    <div v-for="(card, index) in cards" :key="card.flipped" class="group relative">
+                        <!--                        <transition name="flip">-->
+                        <vue-flip active-click="" width="250px" height="400px">
+                            <template v-slot:front class="front w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1
+                                                rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                                <img :src="'../storage/img/' + card.imgSrc" @click="toggleCard(card)"
+                                     alt="product.imageAlt"
+                                     class="mx-4 w-full h-full object-center object-cover lg:w-full lg:h-full"/>
+                                <span class="">click</span>
+                            </template>
+                            <template v-slot:back class="back w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1
+                                                rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                                <img :src="'../storage/img/' + card.imgSrcBack" @click="toggleCard(card)"
+                                     alt="product.imageAlt"
+                                     class="mx-4 w-full h-full object-center object-cover lg:w-full lg:h-full"/>
+                                <span class=""> click</span>
+                            </template>
+                        </vue-flip>
+                        <!--                        </transition>-->
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -44,6 +44,7 @@ import Blog from '@/Pages/Blog.vue';
 
 <script>
 import Painterro from 'painterro';
+import VueFlip from 'vue-flip';
 
 export default {
     data() {
